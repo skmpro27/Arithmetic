c13bd830dac8fddba8863fa0f3959549c4039c39
unknown <kumar.skm03@gmail.com>
2021-02-13 13:53:43 +0530

    added computation.sh

--- computation.sh
+++ computation.sh
@@ -2,6 +2,7 @@
 #! /bin/bash -x
 
 declare -A dict
+declare -a arr
 
 read -p "Enter 1st number " num1
 read -p "Enter 2nd number " num2
@@ -11,3 +12,9 @@ dict[compute1]=$(( num1+num2*num3 ))
 dict[compute2]=$(( num1*num2+num3 ))
 dict[compute3]=$(( num3+num1/num2 ))
 dict[compute4]=$(( num1%num2+num3 ))
+i=0
+for key in ${!dict[@]}
+do
+	arr[$i]=${dict[$key]}
+	i=$(( i+1 ))
+done
