151ad731cc649d98dc825d40613b158735a06f73
unknown <kumar.skm03@gmail.com>
2021-02-13 13:45:43 +0530

    added computation.sh

--- computation.sh
+++ computation.sh
@@ -1,11 +1,13 @@
 
 #! /bin/bash -x
 
+declare -A dict
+
 read -p "Enter 1st number " num1
 read -p "Enter 2nd number " num2
 read -p "Enter 3rd number " num3
 
-compute1=$(( num1+num2*num3 ))
-compute2=$(( num1*num2+num3 ))
-compute3=$(( num3+num1/num2 ))
-compute4=$(( num1%num2+num3 ))
+dict[compute1]=$(( num1+num2*num3 ))
+dict[compute2]=$(( num1*num2+num3 ))
+dict[compute3]=$(( num3+num1/num2 ))
+dict[compute4]=$(( num1%num2+num3 ))
